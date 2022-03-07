3a1eee5840a72b09c2c86f220fb26031fb9f476b
Alisa-InWonderland <alisa_ne@yahoo.com>
2022-03-07 10:26:31 +0100

    carpetas

---
+++ src/main/java/org/factoriaf5/labcoop/LabcoopApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.labcoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabcoopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabcoopApplication.class, args);
+    }
+
+}
