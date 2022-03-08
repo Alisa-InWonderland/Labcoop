1deb036369328d71e801aad06c1ba32ee2dc1d9b
Alisa-InWonderland <alisa_ne@yahoo.com>
2022-03-08 15:39:16 +0100

    cambio diseño

--- frontend/src/Components/ProjectList/index.js
+++ frontend/src/Components/ProjectList/index.js
@@ -14,9 +14,9 @@ export function ProjectList({projects, deleteProject}) {
     }
 
     return (
-            <section className='experienceCatalog'>
-
-                <h1 className="catalog-title">Proyectos</h1>
+        <div className="table-container">
+            <section className='reservesList'>
+                <h1 className="reservesList-title">Proyectos</h1>
 
                 <div className="barra-de-busqueda-container">
                     <input
@@ -29,28 +29,49 @@ export function ProjectList({projects, deleteProject}) {
 
                 </div>
 
-                <div className="container-card">
-                    {projects.map((project) => {
-                        if (
-                            project.name.toLowerCase().indexOf(state.filter) >= 0 ||
-                            state.filter.length === 0
-                        ) {
-                            return <ProjectListItem key={project.id} project={project}
-                                                   deleteProject={deleteProject}/>;
-                        }
-                        if (
-                            project.client.toLowerCase().indexOf(state.filter) >= 0 ||
-                            state.filter.length === 0
-                        ) {
-                            return <ProjectListItem key={project.id} project={project}
-                                                    deleteProject={deleteProject} />;
-                        }
+                <div className='tablelist'>
 
-                        return "";
-                    })}
+                    <table className="reserva-table">
+
+                        <tr className='reserva-table-title'>
+                            <th className='reserva-th'>CODI PROJECTE</th>
+                            <th className='reserva-th'>NOM PROJECTE</th>
+                            <th className='reserva-th'>CLIENT</th>
+                            <th className='reserva-th'>PERSONA COORDINADORA</th>
+                            <th className='reserva-th'>ESTAT</th>
+                            <th className='reserva-th'>TIPO</th>
+                            <th className='reserva-th'>TAMAÑO</th>
+                            <th className='reserva-th'>TARGET</th>
+                            <th className='reserva-th'>MES INICI</th>
+                            <th className='reserva-th'>MES FIN</th>
+                        </tr>
+
+                        {projects.map((project) => {
+                            if (
+                                project.name.toLowerCase().indexOf(state.filter) >= 0 ||
+                                state.filter.length === 0
+                            ) {
+                                return <ProjectListItem key={project.id} project={project}
+                                                       deleteProject={deleteProject}/>;
+                            }
+                            if (
+                                project.client.toLowerCase().indexOf(state.filter) >= 0 ||
+                                state.filter.length === 0
+                            ) {
+                                return <ProjectListItem key={project.id} project={project}
+                                                        deleteProject={deleteProject} />;
+                            }
+
+                            return "";
+                        })}
+
+                    </table>
+
                 </div>
 
             </section>
+
+        </div>
     );
 }
 
--- frontend/src/Components/ProjectListItem/index.js
+++ frontend/src/Components/ProjectListItem/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import "../ProjectList/style.css"
+import "./style.css"
 
 
 
@@ -11,28 +11,22 @@ export function ProjectListItem({project, deleteProject, loggedIn}) {
 
 
     return (
-        <>
-            <div className="experience-card">
-                <div>
-                    <Link to={`/projects/${id}`} state={{data: project}}>
-                        <p className="image" alt="Image of Experience">{code}</p>
-                    </Link>
-                </div>
+        <tr className='reserva-table-row'>
+            <td className="reserva-td"><Link to={`/projects/${id}`} state={{data: project}}>
+                {code}
+            </Link>
+            </td>
+            <td className="reserva-td">{name}</td>
+            <td className="reserva-td">{client}</td>
+            <td className="reserva-td">{manager}</td>
+            <td className="reserva-td">{status}</td>
+            <td className="reserva-td">{type}</td>
+            <td className="reserva-td">{size}</td>
+            <td className="reserva-td">{target}</td>
+            <td className="reserva-td">{startdate}</td>
+            <td className="reserva-td">{enddate}</td>
+        </tr>
 
-                <table className="experience-table">
-                    <tbody>
-                    <tr>
-                        <td>{name}</td>
-                        <td>{client}</td>
-                        <td>{manager}</td>
-                        <td>{status}</td>
-                        <td>{type}</td>
-                    </tr>
-
-                    </tbody>
-                </table>
-            </div>
-        </>
     );
 
 }
