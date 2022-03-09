48ddebe42c2ed6395740e0c78114ef868d79f369
RosaPs <rosapacheco.contact@gmail.com>
2022-03-09 10:07:53 +0100

    Creado método delete

--- src/main/java/org/factoriaf5/labcoop/controllers/ProjectController.java
+++ src/main/java/org/factoriaf5/labcoop/controllers/ProjectController.java
@@ -35,24 +35,28 @@ public class ProjectController {
             return projectsRepository.save(project);
         }
 
-
-
-
     // provisional
     @PostConstruct
     private void cargarDatosDePrueba() {
         projectsRepository.saveAll(
                 List.of(
-                        new Project("21F010", "Emprendoria i Génere 2022", "AJ Montornés Del Vallés", "", "Aprobada", "Contrato", "Mitjant", "Dones", "abr22", "nov22"),
-                        new Project("21F011", "Formació en cures", "Aj Terrassa", "", "Aprovada", "Contracte", "Petit", "cures", "gen22", "març22"),
-                        new Project("21F008", "Formació Socis", "Som Energía", "", "Aprobada", "Contracte", "petit", "ess", "Set22", "Gen22"),
-                        new Project("21F012", "Tallers Viertuals", "Aracoop", "", "Aprovada", "Subvenció", "Gran", "ess", "des21", "oct22"),
-                        new Project("", "Singulars Cooperative Joves", "LabCoop", "", "Aprovada", "Subvensio", "Gran", "joves", "Nov21", "Septembre-22"),
-                        new Project("", "Empendoria verda", "Espai ambiental", "", "Pendent", "Contracte", "Mini", "ess", "", ""),
-                        new Project("", "Formació y emprendimiento verde", "Fundación biodiversidad", "", "Pendent", "Contracte", "Gran", "ess", "Sep21", "Des21"),
-                        new Project("", "Juntes Emprenem amb", "LabCoop", "", "Pendent", "Subvenció", "Mitjant", "dones", "Oct21", "Des21"),
-                        new Project("", "Escola popular d'economia feminista", "Cooperació", "", "Pendent", "Contracte", "petit", "dones", "", "")
+                        new Project("21F010", "Emprendoria i Génere 2022", "AJ Montornés Del Vallés", "", "Aprobada", "Contrato", "Mitjant", "Dones", "abr22", "nov22", ""),
+                        new Project("21F011", "Formació en cures", "Aj Terrassa", "", "Aprovada", "Contracte", "Petit", "cures", "gen22", "març22", ""),
+                        new Project("21F008", "Formació Socis", "Som Energía", "", "Aprobada", "Contracte", "petit", "ess", "Set22", "Gen22", ""),
+                        new Project("21F012", "Tallers Viertuals", "Aracoop", "", "Aprovada", "Subvenció", "Gran", "ess", "des21", "oct22", ""),
+                        new Project("", "Singulars Cooperative Joves", "LabCoop", "", "Aprovada", "Subvensio", "Gran", "joves", "Nov21", "Septembre-22", ""),
+                        new Project("", "Empendoria verda", "Espai ambiental", "", "Pendent", "Contracte", "Mini", "ess", "", "", ""),
+                        new Project("", "Formació y emprendimiento verde", "Fundación biodiversidad", "", "Pendent", "Contracte", "Gran", "ess", "Sep21", "Des21", ""),
+                        new Project("", "Juntes Emprenem amb", "LabCoop", "", "Pendent", "Subvenció", "Mitjant", "dones", "Oct21", "Des21", ""),
+                        new Project("", "Escola popular d'economia feminista", "Cooperació", "", "Pendent", "Contracte", "petit", "dones", "", "", "")
                 ));
+    }
+
+    @DeleteMapping("/project/delete/{id}")
+    public Project deleteProjectById(@PathVariable Long id) {
+        Project project = projectsRepository.findById(id).orElseThrow(ProjectNotFoundException::new);
+        projectsRepository.deleteById(id);
+        return project;
     }
 
 }
