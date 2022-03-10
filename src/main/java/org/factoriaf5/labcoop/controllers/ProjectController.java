0105db093de1e31f409aaeb5cc23872151ace606
AnnaGirona89 <annartworks@gmail.com>
2022-03-10 12:17:05 +0100

    Retoque campos tabla

--- src/main/java/org/factoriaf5/labcoop/controllers/ProjectController.java
+++ src/main/java/org/factoriaf5/labcoop/controllers/ProjectController.java
@@ -42,15 +42,15 @@ public class ProjectController {
     private void cargarDatosDePrueba() {
         projectsRepository.saveAll(
                 List.of(
-                        new Project("21F010", "Emprendoria i Génere 2022", "AJ Montornés Del Vallés", "", "Aprobada", "Contrato", "Mitjant", "Dones", "abr22", "nov22", ""),
-                        new Project("21F011", "Formació en cures", "Aj Terrassa", "", "Aprovada", "Contracte", "Petit", "cures", "gen22", "març22", ""),
-                        new Project("21F008", "Formació Socis", "Som Energía", "", "Aprobada", "Contracte", "petit", "ess", "Set22", "Gen22", ""),
-                        new Project("21F012", "Tallers Viertuals", "Aracoop", "", "Aprovada", "Subvenció", "Gran", "ess", "des21", "oct22", ""),
-                        new Project("", "Singulars Cooperative Joves", "LabCoop", "", "Aprovada", "Subvensio", "Gran", "joves", "Nov21", "Septembre-22", ""),
-                        new Project("", "Empendoria verda", "Espai ambiental", "", "Pendent", "Contracte", "Mini", "ess", "", "", ""),
-                        new Project("", "Formació y emprendimiento verde", "Fundación biodiversitat", "", "Pendent", "Contracte", "Gran", "ess", "Sep21", "Des21", ""),
-                        new Project("", "Juntes Emprenem amb", "LabCoop", "", "Pendent", "Subvenció", "Mitjant", "dones", "Oct21", "Des21", ""),
-                        new Project("", "Escola popular d'economia feminista", "Cooperació", "", "Pendent", "Contracte", "petit", "dones", "", "", "")
+                        new Project("21F010", "Emprendoria i Génere 2022", "Formació", "AJ Montornés Del Vallés", "", "Aprobada", "Contrato", "Mitjant", "Dones", "abr22", "nov22", ""),
+                        new Project("21F011", "Formació en cures", "Formació", "Aj Terrassa", "", "Aprovada", "Contracte", "Petit", "cures", "gen22", "març22", ""),
+                        new Project("21F008", "Formació Socis","Formació", "Som Energía", "", "Aprovada", "Contracte", "petit", "ess", "Set22", "Gen22", ""),
+                        new Project("21F012", "Tallers Viertuals","Formació", "Aracoop", "", "Aprovada", "Subvenció", "Gran", "ess", "des21", "oct22", ""),
+                        new Project("", "Singulars Cooperative Joves","Formació", "LabCoop", "", "Aprovada", "Subvensio", "Gran", "joves", "Nov21", "Septembre-22", ""),
+                        new Project("", "Empendoria verda","P-Territori", "Espai ambiental", "", "Pendent", "Contracte", "Mini", "ess", "", "", ""),
+                        new Project("", "Formació y emprendimiento verde","P-Territori", "Fundación biodiversitat", "", "Pendent", "Contracte", "Gran", "ess", "Sep21", "Des21", ""),
+                        new Project("", "Juntes Emprenem amb","Formació", "LabCoop", "", "Pendent", "Subvenció", "Mitjant", "dones", "Oct21", "Des21", ""),
+                        new Project("", "Escola popular d'economia feminista","Formació", "Cooperació", "", "Pendent", "Contracte", "petit", "dones", "", "", "")
                 ));
     }
 
