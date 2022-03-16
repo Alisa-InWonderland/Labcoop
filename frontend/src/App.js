64d01ad57c956f8e9a3d7c61923223f125405f4e
AnnaGirona89 <annartworks@gmail.com>
2022-03-16 09:34:50 +0100

    foto actual antes de probar Sql

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {useEffect, useState} from "react";
+import {Route, Routes} from "react-router-dom";
+import {getProjects} from "./Services/getProjects";
+import {API_URL} from "./Services/settings";
+import {ProjectList} from "./Components/Projects/ProjectList/index";
+import {ProjectForm} from "./Components/ProjectForm/index";
+// import {Menu} from "./Components/Menu/index"
+// import {Navbar} from "./Components/Navbar";
+
+
+
 function App() {
+
+  const [projects, setProjects] = useState([]);
+  const [requiresUpdate, setRequiresUpdate] = useState(true);
+  /*const [loggedIn, setLoggedIn] = useState(false)*/
+
+
+  useEffect(() => {
+    if (requiresUpdate) {
+      getProjects()
+          .then(setProjects)
+          .then(_ => setRequiresUpdate(false));
+    }
+  }, [requiresUpdate])
+
+
+  const addProject = (project) => {
+    return fetch(API_URL,
+        {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(project)
+        }
+    ).then(_ => setRequiresUpdate(true))
+
+  }
+
+  const deleteProject = (id) => {
+    fetch(`${API_URL}/delete/${id}`,
+        {
+          method: 'DELETE'
+        }
+    ).then(_ => setRequiresUpdate(true))
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+
+        <main className="main">
+          <Routes>
+            <Route path="/projects" element={<ProjectList projects={projects} />}/>
+            <Route path="/projects/:id" element={<ProjectForm addProject={addProject} deleteProject={deleteProject}/>}/>
+            <Route path="/projects/new" element={<ProjectForm addProject={addProject} deleteProject={deleteProject}/>}/>
+          </Routes>
+        </main>
+
+      </div>
   );
 }
 
---
+++ frontend/src/Components/Hours/HoursList/index.js
@@ -0,0 +1,131 @@
+import React from "react";
+import "./style.css"
+import {ProjectListItem} from "../HoursListItem";
+import {Link} from "react-router-dom";
+
+
+
+export function ProjectList({projects}) {
+
+
+    const [state, setState] = React.useState({filter: ""});
+
+    function getData(val) {
+        setState({filter: val.target.value.toLowerCase()});
+    }
+
+    return (
+        <div className="table-container">
+            <section className='reservesList'>
+                <h1 className="reservesList-title">Proyectos</h1>
+
+                <div className="barra-de-busqueda-container">
+                    <input
+                        className="barra-de-busqueda"
+                        name="project"
+                        type="text"
+                        onChange={getData}
+                        placeholder="Busca un proyecto"
+                    />
+
+                        <button className="btn-orange">
+                            <Link className="btn-orange-link" to='/projects/new'>Nuevo proyecto</Link>
+                        </button>
+
+
+                </div>
+
+                <div className='tablelist'>
+
+                    <table className="reserva-table">
+
+                        <tr className='reserva-table-title'>
+                            <th className='reserva-th'>CODI</th>
+                            <th className='reserva-th'>NOM</th>
+                            <th className='reserva-th'>CLIENT</th>
+                            <th className='reserva-th'>COORDINA</th>
+                            <th className='reserva-th'>ESTAT</th>
+                            <th className='reserva-th'>AREA</th>
+                            <th className='reserva-th'>TIPUS</th>
+                            <th className='reserva-th'>MIDA</th>
+                            <th className='reserva-th'>TARGET</th>
+                            <th className='reserva-th'>INICI</th>
+                            <th className='reserva-th'>FI</th>
+                            <th className='reserva-th'>OBSERVACIONS</th>
+                        </tr>
+
+                        {projects.map((project) => {
+                            if (
+                                project.name.toLowerCase().indexOf(state.filter) >= 0 ||
+                                state.filter.length === 0
+                            ) {
+                                return <ProjectListItem key={project.id} project={project}/>;
+                            }
+                            if (
+                                project.client.toLowerCase().indexOf(state.filter) >= 0 ||
+                                state.filter.length === 0
+                            ) {
+                                return <ProjectListItem key={project.id} project={project}/>;
+                            }
+                            if (
+                                project.size.toLowerCase().indexOf(state.filter) >= 0 ||
+                                state.filter.length === 0
+                            ) {
+                                return <ProjectListItem key={project.id} project={project}/>;
+                            }
+                            if (
+                                project.code.toLowerCase().indexOf(state.filter) >= 0 ||
+                                state.filter.length === 0
+                            ) {
+                                return <ProjectListItem key={project.id} project={project}/>;
+                            }
+                            if (
+                                project.manager.toLowerCase().indexOf(state.filter) >= 0 ||
+                                state.filter.length === 0
+                            ) {
+                                return <ProjectListItem key={project.id} project={project}/>;
+                            }
+                            if (
+                                project.target.toLowerCase().indexOf(state.filter) >= 0 ||
+                                state.filter.length === 0
+                            ) {
+                                return <ProjectListItem key={project.id} project={project}/>;
+                            }
+                            if (
+                                project.type.toLowerCase().indexOf(state.filter) >= 0 ||
+                                state.filter.length === 0
+                            ) {
+                                return <ProjectListItem key={project.id} project={project}/>;
+                            }
+                            if (
+                                project.status.toLowerCase().indexOf(state.filter) >= 0 ||
+                                state.filter.length === 0
+                            ) {
+                                return <ProjectListItem key={project.id} project={project}/>;
+                            }
+                            if (
+                                project.comments.toLowerCase().indexOf(state.filter) >= 0 ||
+                                state.filter.length === 0
+                            ) {
+                                return <ProjectListItem key={project.id} project={project}/>;
+                            }
+                            if (
+                                project.area.toLowerCase().indexOf(state.filter) >= 0 ||
+                                state.filter.length === 0
+                            ) {
+                                return <ProjectListItem key={project.id} project={project}/>;
+                            }
+
+                            return "";
+                        })}
+
+                    </table>
+
+                </div>
+
+            </section>
+
+        </div>
+    );
+}
+
