44ae481c884f9050ddd97e7cdb475264bec39d82
Alisa In Wonderland <Alisa@MacBook-Air-Alisa.local>
2022-03-17 13:21:42 +0100

    resolviendo conflicto

--- src/main/java/org/factoriaf5/labcoop/repository/ProjectsRepository.java
+++ src/main/java/org/factoriaf5/labcoop/repository/ProjectsRepository.java
@@ -1,7 +1,5 @@
 package org.factoriaf5.labcoop.repository;
 
-
-import org.factoriaf5.labcoop.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
