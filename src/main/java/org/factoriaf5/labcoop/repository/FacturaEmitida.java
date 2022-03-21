8cecc681255fa8969e02beab29138f93277d5272
RosaPs <rosapacheco.contact@gmail.com>
2022-03-21 08:48:01 +0100

    src/main/java/org/factoriaf5/labcoop/repository/FacturaEmitida.java

--- src/main/java/org/factoriaf5/labcoop/repository/FacturaEmitida.java
+++ src/main/java/org/factoriaf5/labcoop/repository/FacturaEmitida.java
@@ -18,7 +18,6 @@ public class FacturaEmitida {
     private int total;
     private boolean cobrado;
 
-<<<<<<< HEAD
     @OneToOne()
     @JoinTable(name = "projects",
             joinColumns = @JoinColumn(name = "factura_emitida_id"),
@@ -33,8 +32,6 @@ public class FacturaEmitida {
         this.project = project;
     }
 
-=======
->>>>>>> 5bba774503008844194c8a4f54fe449f411a17d9
 
     public FacturaEmitida(int numFactura, String fecha, int importe, int iva, int total, boolean cobrado) {
         this.numFactura = numFactura;
@@ -56,10 +53,6 @@ public class FacturaEmitida {
     public void setId(Long id) {
         this.id = id;
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 5bba774503008844194c8a4f54fe449f411a17d9
     public int getNumFactura() {
         return numFactura;
     }
