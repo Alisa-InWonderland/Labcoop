79dbc15d884f75052cbee5f24794770c506eee37
Alisa In Wonderland <Alisa@MacBook-Air-Alisa.local>
2022-03-24 14:30:44 +0100

    cambio imports

--- frontend/src/Components/ProjectForm/index.js
+++ frontend/src/Components/ProjectForm/index.js
@@ -5,7 +5,7 @@ import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import {GeneralForm} from "../GeneralForm/index";
-import {FacturasEmitidasOfProjectList} from "../FacturasEmitidasOfProject/FacturasEmitidasOfProjectList/index";
+import {FacturaEmitidaForm} from "../FacturaEmitidaForm/index";
 import {HorasForm} from "../HorasForm";
 import * as PropTypes from "prop-types";
 import {FacturasRecibidasView} from "../FacturasRecibidasView/facturasRecibidasView";
@@ -53,7 +53,7 @@ export function ProjectForm({addProject}) {
                     </TabPanel>
 
                     <TabPanel value="2">
-                        <FacturasEmitidasOfProjectList addProject={addProject} value="2"/>
+                        <FacturaEmitidaForm addProject={addProject} value="2"/>
                     </TabPanel>
 
                     <TabPanel value="3">
